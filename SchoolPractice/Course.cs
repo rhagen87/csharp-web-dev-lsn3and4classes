7bfe58c9902f502600c0164a4a65a727b3e28458
rhagen87 <reynold.hagen@gmail.com>
2020-07-10 15:27:31 -0500

    exercises

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+
+        public string name { get; set; }
+        public int credits { get; set; }
+
+        public List<string> students = new List<string>();
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -6,6 +6,11 @@ namespace SchoolPractice
     {
         public static void Main(string[] args)
         {
+            Student renny = new Student();
+            renny.Name = "Renny";
+            renny.StudentId = 1337;
+            renny.NumberOfCredits = 1;
+            renny.Gpa = 4.0;
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
         }
     }
